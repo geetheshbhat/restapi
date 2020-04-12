ff43f087679a9b609bfb695d3db0aa7b480b6059
geethesh17bhat@gmail.com <geethesh17bhat@gmail.com>
2020-04-12 17:47:22 +0530

    add postgres

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_restful import  Api
 from flask_jwt import JWT
@@ -13,7 +14,7 @@ app=Flask(__name__)
 app.secret_key="geethesh"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 api=Api(app)
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI']=os.environ.get('DATABASE_URL','sqlite:///data.db')
 jwt=JWT(app,authenticate,identity)
 
 api.add_resource(Item,'/item/<string:name>')
