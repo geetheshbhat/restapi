9e77fc6837a19928d9d1cb50a6060d19fa463ae3
geethesh17bhat@gmail.com <geethesh17bhat@gmail.com>
2020-04-12 12:20:56 +0530

    Initial Commit

---
+++ resources/user.py
@@ -0,0 +1,20 @@
+import sqlite3
+from flask_restful import Resource, reqparse
+from models.user import UserModel
+
+class UserRegister(Resource):
+    parser=reqparse.RequestParser()
+    parser.add_argument('username',type=str, required=True, help="Enter the username")
+    parser.add_argument('password',type=str,required=True,help="Enter the password")
+    
+    def post(self):
+
+        data=UserRegister.parser.parse_args()
+        if UserModel.find_by_username(data['username']):
+            return {'message':'user with that name already exists'},400
+        
+        data=UserRegister.parser.parse_args()
+        user=UserModel(data['username'],data['password'])
+        user.save_to_db()
+
+        return {"message":"User added successfully"},201
